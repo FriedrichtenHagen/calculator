0d3186bfc6205127cd3d66dec6610cbe542da268
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-09-22 14:36:04 +0900

    Add querySelectors

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ function operate(operator, a, b){
         default: console.log("not valid operator")
     }
 }
+
+const key1 = document.querySelector("one")
+const key2 = document.querySelector("two")
+const key3 = document.querySelector("three")
+const key4 = document.querySelector("four")
+const key5 = document.querySelector("five")
+const key6 = document.querySelector("six")
+const key7 = document.querySelector("seven")
+const key8 = document.querySelector("eight")
+const key9 = document.querySelector("nine")
+const key0 = document.querySelector("zero")
+const keyAdd = document.querySelector("add")
+const keySub = document.querySelector("subtract")
+const keyMul = document.querySelector("multiply")
+const keyDiv = document.querySelector("divide")
+const keyEqu = document.querySelector("equals")
+const keyCle = document.querySelector("clear")
+
